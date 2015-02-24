9de5c8863c450565284a403cb11ffe01bce2946c
Ron Scott-Adams <ron@tohuw.net>
2015-02-23 21:33:35 -0500

    Linting, added .editorconfig file

--- config.rb
+++ config.rb
@@ -2,14 +2,16 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 require 'compass-h5bp'
 
-css_dir = "test/project/css"
-sass_dir = "test/project/sass"
+css_dir = 'test/project/css'
+sass_dir = 'test/project/sass'
 
-# You can select your preferred output style here (can be overridden via the command line):
+# You can select your preferred output style here
+# (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
+# To disable debugging comments that display the
+# original location of your selectors. Uncomment:
 line_comments = false
--- lib/compass/h5bp.rb
+++ lib/compass/h5bp.rb
@@ -1,7 +1,11 @@
-require "compass/h5bp/version"
+require 'compass/h5bp/version'
 
 module Compass
+  # HTML5 Boilerplate v5 Compass Extension
   module Html5bp
-    Compass::Frameworks.register("compass-h5bp", :path => "#{File.dirname(__FILE__)}/../..")
+    Compass::Frameworks.register(
+      'compass-h5bp',
+      path: "#{File.dirname(__FILE__)}/../.."
+    )
   end
 end
